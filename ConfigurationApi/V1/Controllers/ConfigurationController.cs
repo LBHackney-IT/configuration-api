52b5b41fa93ea0c75cd91bcbdbfcb531897b4d99
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-19 15:35:46 +0100

    Setting up

---
+++ ConfigurationApi/V1/Controllers/ConfigurationController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using Hackney.Core.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace ConfigurationApi.V1.Controllers
+{
+    [ApiController]
+    [Route("api/v1/configuration")]
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    public class ConfigurationController : Controller
+    {
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
+        {
+            using (var reader = new StreamReader("JsonFile.txt"))
+            {
+                var result = reader.ReadToEnd();
+
+                return Content(result, "application/json");
+            }
+        }
+    }
+}
