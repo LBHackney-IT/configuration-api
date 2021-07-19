57e1ae813007b68d8a844810fbe71858218559e2
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-07-19 17:09:26 +0100

    Fixed typo in TF

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -24,7 +24,7 @@ terraform {
     bucket  = "terraform-state-development-apis"
     encrypt = true
     region  = "eu-west-2"
-    key     = services/configurations-api/state"
+    key = "services/configurations-api/state"
   }
 }
 
